32abd659340f2c7a2e3a1190573aad45fdcc27d1
phuonghero <32608847+phuonghero@users.noreply.github.com>
2017-12-18 23:06:28 +0700

    Done - Add new marker on map
    hoàn thành function thêm 1 marker vào bản đồ trong trang thông tin chi tiết

--- MyTest/Index/js/myJS/myJS-info.js
+++ MyTest/Index/js/myJS/myJS-info.js
@@ -1,67 +1,86 @@
 var map;
-	var latitude=null;
-    var longitude=null;
-	function initMap() {
-		var directionsService = new google.maps.DirectionsService;
-        var directionsDisplay = new google.maps.DirectionsRenderer;
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 20.996004, lng: 105.808000},
-          zoom: 15
-        });
-        var marker = new google.maps.Marker({
-          map: map,
-          position: {lat: 20.996004, lng: 105.808000},
-        });
-        directionsDisplay.setMap(map);
+var markers = [];
+var latitude=null;
+var longitude=null;
+function initMap() {
+	var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 20.996004, lng: 105.808000},
+    zoom: 15
+  });
+  google.maps.event.addListener(map,'click', function(event) {
+      addMarker(event.latLng);
+  });
+  var marker = new google.maps.Marker({
+    map: map,
+    position: {lat: 20.996004, lng: 105.808000},
+  });
+  directionsDisplay.setMap(map);
+  var onChangeHandler = function() {
+   	calculateAndDisplayRoute(directionsService, directionsDisplay);
+  };
+  document.getElementById('showKC').addEventListener('click', onChangeHandler);
+}
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+  FindMe();
+  directionsService.route({
+    origin: {lat: latitude, lng: longitude},
+    destination: {lat: 20.996004, lng: 105.808000},
+    travelMode: google.maps.TravelMode.DRIVING
+  }, function(response, status) {
+    if (status === google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+      //get direction info
+      var htmlReturn = '';
+      var route = response.routes[0];
+      htmlReturn += "Distance: <strong>" + route.legs[0].distance.text + "</strong>, Duration: <strong>" + route.legs[0].duration.text + "</strong>";
+      document.getElementById('kc').innerHTML  = htmlReturn;
+    } 
+    else {
+        window.alert('Directions request failed due to ' + status);
+    }
+  });
+}
+function FindMe(){
+  if (latitude==null&&longitude==null) {
+    if(navigator.geolocation){
+      navigator.geolocation.watchPosition(selectMyPosition);
+    }
+  }
+  else{
+    map.setCenter({lat:latitude, lng:longitude});
+  }
+}
+function addMarker(location) {
+    var marker = new google.maps.Marker({
+        position: location,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        map: map
+    });
+    markers.push(marker);
+}
+function selectMyPosition(position){
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+}
+function goToThisLocation(){
+  map.setCenter({lat: 20.996004, lng: 105.808000});
+}
 
-        var onChangeHandler = function() {
-         	calculateAndDisplayRoute(directionsService, directionsDisplay);
-        };
-        document.getElementById('showKC').addEventListener('click', onChangeHandler);
+
+
+var n=0;
+function fadeLogin(){
+    if(n==0){
+        $('#login-dp').fadeIn(300);
+        n++;
+    }else{
+        $('#login-dp').fadeOut(300);
+        n--;
     }
-    function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-    	FindMe();
-        directionsService.route({
-          origin: {lat: latitude, lng: longitude},
-          destination: {lat: 20.996004, lng: 105.808000},
-          travelMode: google.maps.TravelMode.DRIVING
-        }, function(response, status) {
-          if (status === google.maps.DirectionsStatus.OK) {
-            directionsDisplay.setDirections(response);
-			//get direction info
-			var htmlReturn = '';
-			var route = response.routes[0];
-			htmlReturn += "Distance: <strong>" + route.legs[0].distance.text + "</strong>, Duration: <strong>" + route.legs[0].duration.text + "</strong>";
-			document.getElementById('kc').innerHTML  = htmlReturn;
-          } else {
-            window.alert('Directions request failed due to ' + status);
-          }
-        });
-    }
-    function FindMe(){
-	    if (latitude==null&&longitude==null) {
-	      if(navigator.geolocation){
-	        navigator.geolocation.watchPosition(selectMyPosition);
-	      }
-	    }
-	    else{
-	      map.setCenter({lat:latitude, lng:longitude});
-	    }
-	}
-    function selectMyPosition(position){
-        latitude = position.coords.latitude;
-        longitude = position.coords.longitude;
-    }
-    var n=0;
-    function fadeLogin(){
-	    if(n==0){
-	        $('#login-dp').fadeIn(300);
-	        n++;
-	    }else{
-	        $('#login-dp').fadeOut(300);
-	        n--;
-	    }
-	}
+}
 $(document).ready(function () {
     var showChar57 = 57;
     $('.hidden-57').each(function() {
