0ee2beb475846c584b2b985e50df403bdfe9802b
TrieuNv <tenlagi16111@gmail.com>
2017-12-24 14:33:47 +0700

    Create data table plans

---
+++ database/migrations/2017_12_24_072703_create_plan_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('subject');
+            $table->text('description');
+            $table->text('note');
+            $table->integer('owner_id')->unsigned();
+            $table->foreign('owner_id')->references('id')->on('users');
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plans');
+    }
+}
